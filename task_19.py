c044b2c2d85e66e709453d70ba9847dab6f3709f
Андрей Н. Мякенький <myakenkii_an@edu.kvantorium-pk.ru>
2020-01-25 15:52:48 +0900

    done

--- task_19.py
+++ task_19.py
@@ -1,12 +1,61 @@
+
+
 #!/usr/bin/python3
+
+
 
 from pyrob.api import *
 
 
+
+def esc():
+
+	while (not wall_is_above()):
+
+		move_up()
+
+	while (not wall_is_on_the_left()):
+
+		move_left()
+
+
+
+
+
 @task
+
 def task_8_29():
-    pass
+
+    while (wall_is_above() and not wall_is_on_the_right()):
+
+    	move_right()
+
+    if (not wall_is_above()):
+
+    	esc()
+
+    else:
+
+    	r = True
+
+    	while (wall_is_above() and not wall_is_on_the_left()):
+
+    		move_left()
+
+    	if (not wall_is_above()):
+
+    		esc()
+
+    	else:
+
+    		while (not wall_is_on_the_right()):
+
+    			move_right()
+
+
+
 
 
 if __name__ == '__main__':
+
     run_tasks()
--- task_20.py
+++ task_20.py
@@ -3,10 +3,45 @@
 from pyrob.api import *
 
 
+
 @task(delay=0.05)
+
 def task_4_3():
-    pass
+
+	for i in range(6):
+
+		move_right()
+
+		while not wall_is_on_the_right():
+
+			fill_cell()
+
+			move_right()
+
+		move_left()
+
+		move_down()
+
+		while not wall_is_on_the_left():
+
+			fill_cell()
+
+			move_left()
+
+		move_down()
+
+	move_right()
+
+
+
+
+
+    	
+
+
+
 
 
 if __name__ == '__main__':
+
     run_tasks()
